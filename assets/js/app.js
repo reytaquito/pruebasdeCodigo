26ac1f2e394a4a8abe5a86136e1ba769e2158d76
Rodrigo Mateos <reytaquito4@gmail.com>
2022-10-28 17:21:42 -0500

    prueba si se sube o no

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('p.name'); //mal lineado se arreglo el linkeamento 
+const $b = document.querySelector('p.blog');
+const $l = document.querySelector('p.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json() //Se agrega el awit con la data definida 
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`; //mal las comillas 
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` //faltaba la $
 }
 
 displayUser('stolinski').catch(handleError);
